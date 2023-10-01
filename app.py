0ae0c8e97029187712a8a0ba37aeac4e0431bae3
Derek <53124417+DerekLW6@users.noreply.github.com>
2023-10-01 15:34:24 +0000

    Uploading, listing and downloading working

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 This module defines a Flask application that allows users to upload files.
 """
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
 
 app = Flask(__name__)
@@ -29,6 +29,16 @@ def upload_file():
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
         return 'File uploaded successfully'
+
+@app.route('/list_files')
+def list_files():
+    # List all files in the UPLOAD_FOLDER
+    uploaded_files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('list_files.html', files=uploaded_files)
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
